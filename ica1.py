4ccc3ea9f143023173ee0886ccec76955a26bf6c
naikprachi1537-arch <naikprachi1537@gmail.com>
2026-08-11 14:55:55 +0530

    Update ica1.py

--- ica1.py
+++ ica1.py
@@ -1,13 +1,13 @@
 #Q-1)Let x=7 and y=2 . print the results x/y and x//y one below the other ,and add a comment stating which operator always gives a float result.
 x=7
 y=2
-#the second operator that is x//y that is floor division always gives a float result
+#the first operator that is x/y that is division always gives a float result
 print( "division=",x/y)
 print("floor division=",x//y)
 
 
 #Q2)Take a sentence as input and print only its first and last character using indexing.
-sentence="welcome to python world"
+sentence=input("enter a sentence")
 print(sentence[0])
 print(sentence[-1])
 
@@ -21,6 +21,6 @@ print("new list=",num2)
 
 #Q4)Write a program to add multiple elements to an existing set in one go using update() with a list of new values.
 s1={1,2,3,4,5}
-s2={6,7,8,9}
-s1.update(s2)
+l1=[6,7,8,9]
+s1.update(l1)
 print("set after adding elements=",s1)
